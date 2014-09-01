af4b25167c007cd00e177a6759b143fb876851c3
Vy Systems - iOS1 <vyios1@Vys-MacBook-Pro.local>
2014-09-01 14:01:05 +0530

    Initial Commit

---
+++ SamplePrescription/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SamplePrescription
+//
+//  Created by Vy Systems - iOS1 on 9/1/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
